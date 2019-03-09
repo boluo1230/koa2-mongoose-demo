b0da2f483900d70507f51f2ae3da60018b070bea
boluo1230 <942261674@qq.com>
2019-03-09 17:19:09 +0800

    修改service

---
+++ service/categoryService.js
@@ -0,0 +1,21 @@
+const CategoryModel = require('../models/category')
+
+class CategoryService {
+  async create (params) {
+    CategoryModel.create(params)
+  }
+
+  async find (query) {
+    return CategoryModel.find({})
+  }
+
+  async findById (cid) {
+    return CategoryModel.findById(cid)
+  }
+
+  async findByIdAndRemove (cid) {
+    CategoryModel.findByIdAndRemove(cid)
+  }
+}
+
+module.exports = new CategoryService()
